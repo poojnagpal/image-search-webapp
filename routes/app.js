26d5f74183089ee68e9cc07de925778af49bf69f
Pooja Nagpal <poojanagpal@berkeley.edu>
2020-04-03 15:20:02 -0700

    deleted some unnecessary files,comments, and css

--- routes/app.js
+++ routes/app.js
@@ -1,28 +1,22 @@
 const express = require('express');
 const router = express.Router();
-
 const imgur = require('../services/imgur');
-
 var path = require('path')
 var app = express()
 app.set('view engine', 'jade')
 app.use(express.static("."));
 
-
 router.get('/', (req, res) => {
     res.sendFile(
-        path.join(__dirname + '/index.html'));
+      path.join(__dirname + '/index.html'));
 });
-
 
 router.get('/search/:q', (req, res) => {
         imgur.getImage(req.params.q, req.query.offset).then(ans => {
-            var json_string = JSON.stringify(ans)
-            console.log(json_string)
-            res.render('page.jade', { layout : 'layout', json: json_string });
+          var json_string = JSON.stringify(ans)
+          res.render('page.jade', { layout : 'layout', json: json_string });
   });
 });
-
 
 router.get('/test', function (req, res) {
     res.render('search_page.pug', { title: 'Hey', message: 'Hello there!' })
